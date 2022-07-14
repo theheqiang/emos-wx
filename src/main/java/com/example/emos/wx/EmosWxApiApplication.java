ff50ea4c958bc1b5185f80bbc11cfa318c816296
Heqiang <1163494766@qq.com>
2022-07-14 22:55:03 +0800

    added xss protection

--- src/main/java/com/example/emos/wx/EmosWxApiApplication.java
+++ src/main/java/com/example/emos/wx/EmosWxApiApplication.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @Slf4j
+@ServletComponentScan
 public class EmosWxApiApplication {
 
     public static void main(String[] args) {
