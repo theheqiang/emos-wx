004843b8def84c91d6736366e027ac6dd7627f6a
Heqiang <1163494766@qq.com>
2022-07-14 22:08:13 +0800

    to validate form fields

--- src/main/java/com/example/emos/wx/controller/TestController.java
+++ src/main/java/com/example/emos/wx/controller/TestController.java
@@ -1,11 +1,12 @@
 package com.example.emos.wx.controller;
 
 import com.example.emos.wx.common.util.R;
+import com.example.emos.wx.controller.form.TestSayHelloForm;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 /**
  * Author: EdisonHo
@@ -16,9 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Api("测试web接口")
 public class TestController {
 
-    @GetMapping("/sayhello")
+    @PostMapping("/sayhello")
     @ApiOperation("the ever more easy method of testing")
-    public R sayHello(){
-        return R.ok().put("message","Helloworld");
+    public R sayHello(@Valid @RequestBody TestSayHelloForm form){
+        return R.ok().put("message","Hello" + form.getName());
     }
 }
