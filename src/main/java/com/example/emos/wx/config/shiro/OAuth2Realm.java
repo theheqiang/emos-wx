36c45951b059d045d0b4139cc340333723253fff
Heqiang <1163494766@qq.com>
2022-07-15 20:16:09 +0800

    Create OAuth2Realm.java

---
+++ src/main/java/com/example/emos/wx/config/shiro/OAuth2Realm.java
@@ -0,0 +1,45 @@
+package com.example.emos.wx.config.shiro;
+
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class OAuth2Realm extends AuthorizingRealm {
+    @Autowired
+    private JwtUtil jwtUtil;
+
+
+    @Override
+    public boolean supports(AuthenticationToken token) {
+        return token instanceof OAuth2Token;
+    }
+
+    /**
+     * 授权(验证权限时调用)
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection collection) {
+
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+
+        return info;
+    }
+
+    /**
+     * 认证(验证登录时调用)
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo();
+        return info;
+    }
+}
